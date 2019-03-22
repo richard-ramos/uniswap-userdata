f7f09de2379f4d13081f9be9e6aad851f5e7fc02
Richard Ramos <info@richardramos.me>
2019-03-21 23:46:14 -0400

    add cache to apollo client

--- src/api/apollo.js
+++ src/api/apollo.js
@@ -1,7 +1,11 @@
 import ApolloClient from "apollo-boost";
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  uri: "https://api.thegraph.com/subgraphs/name/graphprotocol/uniswap"
+  uri: "https://api.thegraph.com/subgraphs/name/graphprotocol/uniswap",
+  cache
 });
 
 export default client;
