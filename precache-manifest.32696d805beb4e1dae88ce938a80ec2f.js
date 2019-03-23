8e3a650719d10e52141b8e90bcdd76bb4fa5d396
Richard Ramos <info@richardramos.me>
2019-03-23 07:58:31 -0400

    Updates

---
+++ precache-manifest.32696d805beb4e1dae88ce938a80ec2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23447dfcbc56926ddc78",
+    "url": "/uniswap-userdata/static/js/runtime~main.39014259.js"
+  },
+  {
+    "revision": "ce7c40ea086547fbb1ba",
+    "url": "/uniswap-userdata/static/js/main.792c9e26.chunk.js"
+  },
+  {
+    "revision": "b9226fe4c8666a3d9f9d",
+    "url": "/uniswap-userdata/static/js/2.5f40fa33.chunk.js"
+  },
+  {
+    "revision": "ce7c40ea086547fbb1ba",
+    "url": "/uniswap-userdata/static/css/main.fae68f71.chunk.css"
+  },
+  {
+    "revision": "cf9e1bc316ef3b5dfed76ebc4dbf526d",
+    "url": "/uniswap-userdata/index.html"
+  }
+];
