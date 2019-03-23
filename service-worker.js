7f3cee4e7ec25e497621dea1cde9dd014b57df70
Richard Ramos <info@richardramos.me>
2019-03-23 11:03:50 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/uniswap-userdata/precache-manifest.32696d805beb4e1dae88ce938a80ec2f.js"
+  "/uniswap-userdata/precache-manifest.0a7d0378f1c52d512d88a92b9c17636b.js"
 );
 
 workbox.clientsClaim();
